83b86eacd54e441da49dc880ef0d620e26ecd575
aumsolanki06 <aumnanji1092006@gmail.com>
2026-05-21 15:08:50 +0530

    21 may

--- lib/models/complaint.dart
+++ lib/models/complaint.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 class Complaint {
   final String id;
   final String room;
@@ -6,7 +8,7 @@ class Complaint {
   String description;
   final String category;
   final String timeAgo;
-  String status; 
+  String status;
 
   Complaint({
     required this.id,
@@ -14,8 +16,40 @@ class Complaint {
     required this.block,
     required this.title,
     required this.description,
-    required this.category, 
+    required this.category,
     required this.timeAgo,
     required this.status,
   });
+
+  // --- ADDED FOR SHARED PREFERENCES ---
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'room': room,
+      'block': block,
+      'title': title,
+      'description': description,
+      'category': category,
+      'timeAgo': timeAgo,
+      'status': status,
+    };
+  }
+
+  factory Complaint.fromMap(Map<String, dynamic> map) {
+    return Complaint(
+      id: map['id'] ?? '',
+      room: map['room'] ?? '',
+      block: map['block'] ?? '',
+      title: map['title'] ?? '',
+      description: map['description'] ?? '',
+      category: map['category'] ?? '',
+      timeAgo: map['timeAgo'] ?? '',
+      status: map['status'] ?? '',
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory Complaint.fromJson(String source) => Complaint.fromMap(json.decode(source));
 }
